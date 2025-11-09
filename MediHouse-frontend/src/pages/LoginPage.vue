feb4db67424df6d265711f76c7f607fc432b689a
JackReinald <antoniojrvictoria@gmail.com>
2025-11-08 22:09:55 -0500

    feat: Restructure project root for monorepo setup
    Moves the .git, .gitignore, and .gitattributes directories one level up to establish the parent folder as the new repository root.
    
    This allows the Frontend project (Quasar) to be tracked, clearly separating the two parts of the project:
    
    - MediHouse-backend/ (Spring Boot)
    
    - MediHouse-frontend/ (Quasar)
    
    The .gitignore file is updated to apply exclusion rules at the repository level and include Node/Quasar build artifacts (node_modules, .quasar, dist/, etc.).

---
+++ MediHouse-frontend/src/pages/LoginPage.vue
@@ -0,0 +1,79 @@
+<template>
+  <q-page class="q-pa-md">
+    <q-form class="q-gutter-lg" @submit="onSubmit" @reset="onReset">
+      <q-input
+        v-model="email"
+        filled
+        label="Ingrese su correo electrónico"
+        placeholder="mary@domain.com"
+        lazy-rules
+        :rules="[(val) => (val && val.length > 0) || 'Por favor ingrese su correo']"
+      />
+      <q-input
+        v-model="password"
+        filled
+        :type="isPwd ? 'password' : 'text'"
+        placeholder="aguacate1234"
+        label="Ingrese su contraseña"
+        lazy-rules
+        :rules="[
+          (val) => (val && val.length >= 10) || '1. La clave debe tener al menos 10 caracteres.',
+          (val) => /[A-Z]/.test(val) || '2. La clave debe contener al menos 1 mayúscula.',
+          (val) => /\d/.test(val) || '3. La clave debe contener al menos 1 número.',
+        ]"
+      >
+        <template v-slot:append>
+          <q-icon
+            :name="isPwd ? 'visibility_off' : 'visibility'"
+            class="cursor-pointer"
+            @click="isPwd = !isPwd"
+          />
+        </template>
+      </q-input>
+
+      <div class="q-mt-md">
+        <q-btn type="submit" icon="mail">Enviar</q-btn>
+        <q-btn type="reset" icon="clear_all" hint="something123">Limpiar todo</q-btn>
+      </div>
+    </q-form>
+
+    <q-toggle v-model="accept" label="¿El servidor respondió bien?" />
+  </q-page>
+</template>
+
+<script setup>
+import { useQuasar } from 'quasar'
+import { ref } from 'vue'
+const $q = useQuasar()
+
+const email = ref(null)
+const password = ref(null)
+const isPwd = ref(true)
+const accept = ref(false)
+
+const onSubmit = () => {
+  if (accept.value) {
+    console.log('Entró al condicional de aceptado')
+    $q.notify({
+      color: 'green-4',
+      textColor: 'white',
+      icon: 'cloud_done',
+      message: 'Submitted',
+    })
+  } else {
+    console.log('Entró al condicional de rechazado')
+
+    $q.notify({
+      color: 'red-4',
+      textColor: 'white',
+      icon: 'error',
+      message: 'Not Submitted',
+    })
+  }
+}
+const onReset = () => {
+  email.value = null
+  password.value = null
+  isPwd.value = null
+}
+</script>
